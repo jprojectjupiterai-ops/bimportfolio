26e0de278bae54f1cd0615fcf5426114d4167cf0
John Paul Maulion <maulion16john@gmail.com>
2026-05-18 13:51:21 +0800

    Exclude public folder from Vercel Serverless Function tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['public/**/*'],
+    },
+  },
+};
 
 export default nextConfig;
